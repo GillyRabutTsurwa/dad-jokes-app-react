205aaa37e8e20c7f3c3d1f4121fb7e7c03f20fde
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2022-07-15 13:31:59 -0400

    Joke Votes Functionality - this commit is rich - we added a votes property to our joke objects in order to be able to upvote and downvote jokes - we wrote a function that handles votes for each joke: handleVote()   - i also learnt that there is a different between calling a function from an event handler like this: fn(param) vs {() => fn(param)} - i don't know what the difference is yet, but i know that there is a difference and it is very important - added two method props that are responsible for calling our handleVote(): upVote and downVote

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,7 +1,25 @@
 const Joke = (props) => {
-  const { joke } = props;
+  // NOTE: now passing the entire joke object as a prop...
+  const { joke, upVote, downVote } = props;
 
-  return <li>{joke}</li>;
+  /**
+   * NOTE:
+   * and thus i am destructuring it again
+   * also, i am giving the joke property a new name to use: text for extra clarity...
+   * ...because a joke property from an object with the same name is a bit confusing
+   */
+  const { joke: text, votes } = joke;
+
+  return (
+    <li>
+      <div className="joke-buttons">
+        <i className="fas fa-arrow-up" onClick={upVote} />
+        <span>{votes}</span>
+        <i className="fas fa-arrow-down" onClick={downVote} />
+      </div>
+      <div className="joke-text">{text}</div>
+    </li>
+  );
 };
 
 export default Joke;
--- src/Jokes.jsx
+++ src/Jokes.jsx
@@ -18,19 +18,55 @@ const Jokes = () => {
 
       console.log(response);
       const data = response.data;
-      setJokes(data.results);
+      const rigolos = data.results.map((currentRigolo) => {
+        return {
+          /**
+           * NOTE:
+           * ici, on veut ajouter un propietaire "custom" qui ne se trouve pas, par defaut, à l'objet
+           * on va donc utiliser le "spread operator", pour appliquer les propietaires existantes dans un nouvel objet
+           * et aussi ajouter un nouveau propietaire (votes)
+           * on verifiant au console, ça marche bien
+           */
+          ...currentRigolo,
+          votes: 0,
+        };
+      });
+      setJokes(rigolos);
     };
 
     fetchData();
   }, []);
 
-  console.log(jokes);
+  const handleVote = (id, delta = 1) => {
+    const jokesWithVotes = jokes.map((currentJoke) => {
+      return currentJoke.id === id ? { ...currentJoke, votes: currentJoke.votes + delta } : currentJoke;
+    });
+    setJokes(jokesWithVotes);
+  };
 
   return (
     <div className="jokes-container">
       <ul className="jokes__list">
         {jokes.map((currentJoke) => (
-          <Joke key={currentJoke.id} joke={currentJoke.joke} />
+          // NOTE: this didn't work. this is why: https://stackoverflow.com/questions/64626366/why-does-setting-state-in-child-component-onclick-cause-recursive-loop-even-when
+          // <Joke key={currentJoke.id} joke={currentJoke} upVote={handleVote(currentJoke.id)} />
+
+          //   NOTE: this works comme l'on veut
+          <Joke
+            key={currentJoke.id}
+            joke={currentJoke}
+            /**
+             * NOTE i'm not adding (+)1 as the second parametre here, as I do with downvote because I have 1 as the default parametre
+             * so with the default parametre (see actual function), i am not doing this: handleVote(currentJoke.id, 1)
+             * 1 is the default parametre, so if no parametre is specified, 1 will be implicitly applied
+             */
+            upVote={() => {
+              handleVote(currentJoke.id);
+            }}
+            downVote={() => {
+              handleVote(currentJoke.id, -1);
+            }}
+          />
         ))}
       </ul>
     </div>
